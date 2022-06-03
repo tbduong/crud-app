86afb18c1743b075edd594a38a9d3a1a05d79a9c
tbduong <tiffaniduong@gmail.com>
2022-06-02 23:05:26 -0700

    add bodyparser module

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 const app = express(); //Initialize app variable as express application
+const bodyparser = require('body-parser');
 
 dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080  //if value of this PORT value from env file is not available, point to port 8080
 //nodemon server.js
+
+//log requests on console whenever we make a request
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true}));
+
+//set view engine
+
 
 app.get('/', (req, res) => {
     res.send("Crud Application Initialized");
