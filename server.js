e0a6fd42aabeaf41494a1322c963de7539415ccb
tbduong <tiffaniduong@gmail.com>
2022-06-03 01:29:03 -0700

    add Show schema and CRUD logic

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080  //if value of this PORT value from env file is not available, point to port 8080
 //nodemon server.js
 
-mongoose.connect('mongodb://localhost/meanDb', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/meanDb');
 
 //ROOT
 app.get('/', (req, res) => {
     res.send("Crud Application Initialized");
 })
 
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 //Call on routing from appRoutes file
 app.use('/', appRoutes);
 
 app.listen(PORT, () => {console.log(`Server is running on ${PORT}`)});
+
+//mongod (mongod --dbpath ~/documents/data/db)
--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -1,21 +1,55 @@
 const express = require('express');
 var router = express.Router(); //defintes the different http methods
+var Show = require('../models/Schema');
 
 //e.g When a client send a post request with URI create, execute this method 
 router.post('/create', (req, res, next)=>{ 
-    res.status(200).json({msg: 'POST request is working'});
+    var newShow = new Show({
+        title: req.body.title,
+        platform: req.body.platform
+    });
+
+    newShow.save((err, show) => {
+        if (err) {
+            res.status(500).jsom({ errmsg:err });
+        res.status(200).json({ msg: show });
+        }
+    });
+    // res.status(200).json({msg: 'POST request is working'});
 });
 
 router.get('/read', (req, res, next)=>{ 
-    res.status(200).json({msg: 'GET request is working'});
+    Show.find({ }, (err, shows) => {
+        res.status(500).jsom({ errmsg:err });
+        res.status(200).json({ msg: shows });
+    });
+    // res.status(200).json({msg: 'GET request is working'});
 });
 
 router.put('/update', (req, res, next)=>{ 
-    res.status(200).json({msg: 'PUT/UPDATE request is working'});
+
+    Shows.findById(req.body._id, (err, show)=>{
+        if (err) {
+            res.status(500).jsom({ errmsg:err });
+        show.title = req.body.title;
+        show.platform = req.body.platform;
+        show.save((err, show)=> {
+            res.status(500).jsom({ errmsg:err });
+        res.status(200).json({ msg: shows });
+        })
+        }
+    });
+    // res.status(200).json({msg: 'PUT/UPDATE request is working'});
 });
 
 router.delete('/delete/:id', (req, res, next)=>{ 
-    res.status(200).json({msg: 'DELETE request is working'});
+    Show.findOneAndRemove({ _id:req.params._id}, (err, show) =>{
+        if (err) {
+            res.status(500).jsom({ errmsg:err });
+        res.status(200).json({ msg: show });
+        }
+    });
+    // res.status(200).json({msg: 'DELETE request is working'});
 });
 
 module.exports = router;
